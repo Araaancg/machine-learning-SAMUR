ce1f757affed66e25b3a5cb4ca69df183bd9898f
Arancha Carpintero Guardiola <carpinteroaranchag@gmail.com>
2024-04-24 00:13:46 +0200

    feat: first touches with data

--- utils/transformDf.py
+++ utils/transformDf.py
@@ -2,18 +2,18 @@ import pandas as pd
 from incidentData import codes, districts, hospitals
 
 monthMap = {
-    "ENERO": "01",
-    "FEBRERO": "02",
-    "MARZO": "03",
-    "ABRIL": "04",
-    "MAYO": "05",
-    "JUNIO": "06",
-    "JULIO": "07",
-    "AGOSTO": "08",
-    "SEPTIEMBRE": "09",
-    "OCTUBRE": "10",
-    "NOVIEMBRE": "11",
-    "DICIEMBRE": "12"
+  "ENERO": "01",
+  "FEBRERO": "02",
+  "MARZO": "03",
+  "ABRIL": "04",
+  "MAYO": "05",
+  "JUNIO": "06",
+  "JULIO": "07",
+  "AGOSTO": "08",
+  "SEPTIEMBRE": "09",
+  "OCTUBRE": "10",
+  "NOVIEMBRE": "11",
+  "DICIEMBRE": "12"
 }
 
 def transformDf(df, name):
